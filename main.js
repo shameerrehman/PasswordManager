cc8113c7bad812626c336f81509991de67e85a4a
Shameer Rehman <shameerrehman.inbox@gmail.com>
2022-12-21 23:12:07 -0500

    added signup/login modals

---
+++ main.js
@@ -0,0 +1,18 @@
+//display modal on click
+const modalWrapper = document.querySelector(".modals-wrapper");
+if (modalWrapper){
+    function displayModal(id){
+        const modal = document.getElementById(id);
+        modalWrapper.style.display = "flex";
+        modal.style.display = "flex";
+
+        //close modal
+        const close=document.getElementById('close-modal')
+        close.addEventListener("click", ()=>{
+            modalWrapper.style.display = "none";
+            modal.style.display = "none";
+        })
+    }
+}
+
+
