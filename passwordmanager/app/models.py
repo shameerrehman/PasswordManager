f672f7419363b73fc0f6d17834271a4f9fe70faa
Shameer Rehman <shameerrehman.inbox@gmail.com>
2022-12-26 21:16:53 -0500

    fixed error with loggin out, adding logic for confirming login with sending code to email, and adding new password to DB

--- passwordmanager/app/models.py
+++ passwordmanager/app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Password(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owner")
+    email = models.CharField(max_length=200)
+    password = models.CharField(max_length=200)
+    title = models.CharField(max_length=100)
+    favicon = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ["-id"]
--- passwordmanager/app/views.py
+++ passwordmanager/app/views.py
@@ -4,6 +4,19 @@ from django.conf import settings
 from django.contrib import messages
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+import random
+from django.core.mail import send_mail
+from cryptography.fernet import Fernet
+from mechanize import Browser
+import favicon
+from .models import Password
+
+import logging.config
+logger = logging.getLogger(__name__)
+
+browser = Browser()
+browser.set_handle_robots(False)
+fernet = Fernet(settings.KEY)
 
 def index(req):
     if req.method  == "POST":
@@ -37,12 +50,12 @@ def index(req):
                     messages.success(req, notif)
                     return HttpResponseRedirect(req.path)
 
-        if "logout" in req.POST:
-            logout(req)
+        elif "logout" in req.POST:
             notif = f"Logout successful, see you next time!"
+            logout(req)
             messages.success(req, notif)
             return HttpResponseRedirect(req.path)
-        if "login" in req.POST:
+        elif "login" in req.POST:
             username = req.POST.get("username")
             password = req.POST.get("password")
             new_login = authenticate(req, username=username, password=password)
@@ -50,5 +63,57 @@ def index(req):
                 notif = f"Login unsuccessful, invalid credentials."
                 messages.error(req, notif)
                 return HttpResponseRedirect(req.path)
+            else:
+                code = str(random.randint(100000, 999999))
+                global global_code
+                global_code = code
+                send_mail(
+                    "SafePass Manager: Confirm Your Email",
+                    f"Your verification code is: {code}. Dont share this code with others.",
+                    settings.EMAIL_HOST_USER,
+                    [new_login.email],
+                    fail_silently=False,
+                )
+                return render(req, "index.html", 
+                {
+                    'code':code,
+                    'user':new_login
+                })
+        elif "confirm" in req.POST:
+            code_input = req.POST.get('code')
+            user = req.POST.get('user')
+            if code_input != global_code:
+                notif = "Wrong code, please try logging in again."
+                messages.error(req, notif)
+                return HttpResponseRedirect(req.path)
+            else:
+                login(req, User.objects.get(username=user))
+                notif = f"{req.user} welcome back!"
+                messages.success(req, notif)
+                return HttpResponseRedirect(req.path)
+
+        elif "add-new-pass" in req.POST:
+            url = req.POST.get('url')
+            email = req.POST.get('email')
+            password = req.POST.get('password')
+            #encrypt user secrets
+            encrypted_email = fernet.encrypt(email.encode())
+            encrypted_password = fernet.encrypt(password.encode())
+            #get url details
+            browser.open(url)
+            site_title = browser.title()
+            #get favicon
+            site_favicon = favicon.get(url)[0].url
+            #save to DB
+            new_password_entry = Password.objects.create(
+                user = req.user,
+                email = encrypted_email.decode(),
+                password = encrypted_password.decode(),
+                title = site_title,
+                favicon = site_favicon
+            )
+            notif = f"{site_title} successfully added!"
+            messages.success(req, notif)
+            return HttpResponseRedirect(req.path)
 
     return render(req, "index.html", {})
